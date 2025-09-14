b7d620481c8d82d3e2757bef45223227229ca47d
estibenlicona <estibenlicona@gmail.com>
2025-09-14 02:09:07 -0500

    feat: detect SNYK_PATH dynamically from system PATH

--- config.py
+++ config.py
@@ -1,17 +1,33 @@
 # config.py
 # Configuraciones y constantes para el proyecto
 
+
+
 import os
+import shutil
+from dotenv import load_dotenv
+load_dotenv()
 
+def get_env_var(name):
+	value = os.getenv(name)
+	if value is None:
+		raise RuntimeError(f"Variable de entorno '{name}' no definida en .env")
+	return value
 
-# Ruta completa a Snyk en Windows
-SNYK_PATH = r'C:\Users\estib\AppData\Local\Volta\bin\snyk.cmd'
+def find_snyk_path():
+	snyk_path = shutil.which("snyk")
+	if snyk_path is None:
+		raise RuntimeError("No se encontró el ejecutable 'snyk' en el PATH del sistema.")
+	return snyk_path
+
+# Ruta completa a Snyk detectada dinámicamente
+SNYK_PATH = find_snyk_path()
 
 # Organización Snyk parametrizable
-SNYK_ORG = os.getenv("SNYK_ORG", "estibenlicona")
+SNYK_ORG = get_env_var("SNYK_ORG")
 
 # Carpeta que contiene los archivos de entrada para el escaneo
-SCAN_FOLDER = 'scan_input'
+SCAN_FOLDER = get_env_var("SCAN_FOLDER")
 
 # Nombre del archivo de dependencias en la carpeta de escaneo
-REQUIREMENTS_FILE = 'requirements.txt'
+REQUIREMENTS_FILE = get_env_var("REQUIREMENTS_FILE")
